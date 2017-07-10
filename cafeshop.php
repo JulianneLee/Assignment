5aeffb08d330ea225eef937e6c1c7db587576fc8
JulianneLee <juliannelrp@yahoo.com>
2017-07-10 11:02:58 +0800

    Add files via upload

---
+++ cafeshop.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "<h1>Your email has been received!</h1>";
+echo "<h2>We will be sending you notification shortly. Thank you.</h2>";
+?> 
+
+</body>
+</html>
